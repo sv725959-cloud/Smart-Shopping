6cd976f102125a4c4c7a47a459eaa72556d82a2b
sv725959-cloud <sv725959@gmail.com>
2026-05-24 15:46:28 +0530

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = "https://szpnmkwdgklglkbsfnah.supabase.co";
+const SUPABASE_KEY = "sb_publishable_olOpQ5by5lYy93IXO5RCzw_Oex3xtTW";
+
+// Placeholder (real integration later)
+console.log("Supabase Connected");
